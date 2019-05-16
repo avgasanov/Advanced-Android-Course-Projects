64666d5dd6af1a76ca47bcb13ddd2fd9b75d4ecc
AVGasanov <avgasanov@gmail.com>
2019-05-16 23:18:20 +0400

    Create the ScreenNavigator

--- First/app/src/main/java/com/guess_apps/advancedandroid/home/MainActivityComponent.java
+++ First/app/src/main/java/com/guess_apps/advancedandroid/home/MainActivityComponent.java
@@ -1,6 +1,7 @@
 package com.guess_apps.advancedandroid.home;
 
 import com.guess_apps.advancedandroid.di.ActivityScope;
+import com.guess_apps.advancedandroid.ui.NavigationModule;
 
 import dagger.Subcomponent;
 import dagger.android.AndroidInjector;
@@ -8,6 +9,7 @@ import dagger.android.AndroidInjector;
 @ActivityScope
 @Subcomponent(modules = {
         MainScreenBindingModule.class,
+        NavigationModule.class,
 })
 public interface MainActivityComponent extends AndroidInjector<MainActivity> {
 
