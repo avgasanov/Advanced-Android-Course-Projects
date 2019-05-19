9d477b1014e12e507f468b3482df4eff4caf7ef3
AVGasanov <avgasanov@gmail.com>
2019-05-19 21:37:50 +0400

    Add repo details view model

---
+++ First/app/src/main/java/com/guess_apps/advancedandroid/details/ContributorState.java
@@ -0,0 +1,36 @@
+package com.guess_apps.advancedandroid.details;
+
+import com.google.auto.value.AutoValue;
+import com.guess_apps.advancedandroid.model.Contributor;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+@AutoValue
+abstract class ContributorState {
+
+    abstract boolean loading();
+
+    @Nullable
+    abstract List<Contributor> contributors();
+
+    @Nullable
+    abstract Integer errorRes();
+
+    static Builder builder() {
+        return new AutoValue_ContributorState.Builder();
+    }
+
+    @AutoValue.Builder
+    abstract static class Builder {
+
+        abstract Builder loading(boolean loading);
+
+        abstract Builder contributors(List<Contributor> contributors);
+
+        abstract Builder errorRes(Integer errorRes);
+
+        abstract ContributorState build();
+    }
+}
