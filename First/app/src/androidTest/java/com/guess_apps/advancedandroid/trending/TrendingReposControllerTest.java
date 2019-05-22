dc5df93838b2c6419b47ad9412e3a57b9443f180
AVGasanov <avgasanov@gmail.com>
2019-05-22 22:55:35 +0400

    Enhance the Test Repo Service

--- First/app/src/androidTest/java/com/guess_apps/advancedandroid/trending/TrendingReposControllerTest.java
+++ First/app/src/androidTest/java/com/guess_apps/advancedandroid/trending/TrendingReposControllerTest.java
@@ -33,7 +33,7 @@ public class TrendingReposControllerTest extends ControllerTest{
 
     @Test
     public void loadRepos() {
-        repoService.setSendError(false);
+        repoService.clearErrorFlags();
         launch();
 
         onView(withId(R.id.loading_indicator)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
@@ -45,7 +45,7 @@ public class TrendingReposControllerTest extends ControllerTest{
 
     @Test
     public void loadReposError() {
-        repoService.setSendError(true);
+        repoService.setErrorFlags(TestRepoService.FLAG_TRENDING_REPOS);
         launch();
 
         onView(withId(R.id.loading_indicator)).check(matches(withEffectiveVisibility(ViewMatchers.Visibility.GONE)));
