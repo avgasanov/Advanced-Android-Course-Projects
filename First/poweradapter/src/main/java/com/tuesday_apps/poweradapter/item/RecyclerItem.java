ed269892baa52a913a379f35b72d7b4c68b7ef43
AVGasanov <avgasanov@gmail.com>
2019-05-25 00:45:35 +0400

    Create interfaces for power adapter

---
+++ First/poweradapter/src/main/java/com/tuesday_apps/poweradapter/item/RecyclerItem.java
@@ -0,0 +1,8 @@
+package com.tuesday_apps.poweradapter.item;
+
+public interface RecyclerItem {
+
+    long getId();
+
+    String renderKey();
+}
---
+++ First/poweradapter/src/main/java/com/tuesday_apps/poweradapter/item/RenderKey.java
@@ -0,0 +1,13 @@
+package com.tuesday_apps.poweradapter.item;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+import dagger.MapKey;
+
+@MapKey
+@Target(ElementType.METHOD)
+public @interface RenderKey {
+
+    String value();
+}
